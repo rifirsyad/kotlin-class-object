aa417768304720d2e8c1f69dfca33ff4680ef26c
rifirsyad <rifirsyad@gmail.com>
2020-12-30 00:51:14 +0700

    Create Orang.kt

---
+++ inheritance/Orang.kt
@@ -0,0 +1,12 @@
+package inheritance
+
+open class Orang {
+    var nik: String = ""
+    var nama: String = ""
+    var alamat: String = ""
+
+    fun perkenalan(){
+        println("Hallo, nama saya $nama, alamat saya di $alamat")
+    }
+
+}
