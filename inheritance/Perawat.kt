eb6f301fc3f5c72ae945c4324e975f20ebb87e58
rifirsyad <rifirsyad@gmail.com>
2021-01-02 13:06:27 +0700

    Create Perawat.kt

--- inheritance/Perawat.kt
+++ inheritance/Perawat.kt
@@ -1,10 +1,7 @@
 package inheritance
-	
-	class Perawat : Karyawan() {
-	
-	    var klinik: String = ""
-	
-	    fun bekerja(){
-	        println("Saya bekerja di $klinik")
-	    }
+class Perawat : Karyawan() {
+	var klinik: String = ""
+	fun bekerja() {
+		println("Saya bekerja di $klinik")
 	}
+}
