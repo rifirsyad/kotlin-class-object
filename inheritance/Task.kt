bbb2613efd98ad344d07ef613fcdf43585e70bda
rifirsyad <rifirsyad@gmail.com>
2021-01-02 16:42:02 +0700

    Create Task.kt

--- inheritance/Task.kt
+++ inheritance/Task.kt
@@ -27,56 +27,87 @@ fun main() {
     budi.menjaga()
     
     val adi = Pilot()
-    budi.nama = "Adi Prasetyo"
-    budi.alamat = "Sleman"
-    budi.perkenalan()
-    budi.gaji = 5000000
-    budi.bekerja()
-    budi.maskapai = "Lion Air"
-    budi.bekerja()
+    adi.nama = "Adi Prasetyo"
+    adi.alamat = "Sleman"
+    adi.perkenalan()
+    adi.gaji = 5000000
+    adi.bekerja()
+    adi.maskapai = "Lion Air"
+    adi.bekerja()
     
     val nina = Perawat()
-    budi.nama = "Nurlina Nina"
-    budi.alamat = "Pati"
-    budi.perkenalan()
-    budi.gaji = 200000
-    budi.bekerja()
-    budi.klinik = "Sehat Sentosa"
-    budi.bekerja()
+    nina.nama = "Nurlina Nina"
+    nina.alamat = "Pati"
+    nina.perkenalan()
+    nina.gaji = 200000
+    nina.bekerja()
+    nina.klinik = "Sehat Sentosa"
+    nina.bekerja()
     
     val mina = Pramugari()
-    budi.nama = "Mina Lestari"
-    budi.alamat = "Jepara"
-    budi.perkenalan()
-    budi.gaji = 3000000
-    budi.bekerja()
-    budi.maskapai = "Air Asia"
-    budi.bekerja()
+    mina.nama = "Mina Lestari"
+    mina.alamat = "Jepara"
+    mina.perkenalan()
+    mina.gaji = 3000000
+    mina.bekerja()
+    mina.maskapai = "Air Asia"
+    mina.bekerja()
     
     val arka = Dokter()
-    budi.nama = "Arkana Mahesa"
-    budi.alamat = "Jakarta"
-    budi.perkenalan()
-    budi.gaji = 4000000
-    budi.bekerja()
-    budi.klinik = "Sehat Mulia"
-    budi.bekerja()
+    arka.nama = "Arkana Mahesa"
+    arka.alamat = "Jakarta"
+    arka.perkenalan()
+    arka.gaji = 4000000
+    arka.bekerja()
+    arka.klinik = "Sehat Mulia"
+    arka.bekerja()
     
     val dito = Chef()
-    budi.nama = "Dito Nugroho"
-    budi.alamat = "Palembang"
-    budi.perkenalan()
-    budi.gaji = 3500000
-    budi.memasak()
-    budi.restoran = "Kenyang Nikmat"
-    budi.memasak()
+    dito.nama = "Dito Nugroho"
+    dito.alamat = "Palembang"
+    dito.perkenalan()
+    dito.gaji = 3500000
+    dito.memasak()
+    dito.restoran = "Kenyang Nikmat"
+    dito.memasak()
     
     val arman = Polisi()
-    budi.nama = "Arman Pradityo"
-    budi.alamat = "Bali"
-    budi.perkenalan()
-    budi.gaji = 2500000
-    budi.menjaga()
-    budi.polsek = "Kuta"
-    budi.menjaga()
+    arman.nama = "Arman Pradityo"
+    arman.alamat = "Bali"
+    arman.perkenalan()
+    arman.gaji = 2500000
+    arman.menjaga()
+    arman.polsek = "Kuta"
+    arman.menjaga()
+    
+    val dimas = Guru()
+    dimas.nama = "dimas nugroho"
+    dimas.alamat = "Bantul"
+    dimas.perkenalan()
+    dimas.gaji = 250000
+    dimas.bekerja()
+    dimas.mapel = "Sejarah"
+    dimas.mengajar()
+    
+    val mukti = KaryawanSekolah()
+    mukti.nama = "mukti farhan"
+    mukti.alamat = "klaten"
+    mukti.perkenalan()
+    mukti.gaji = 1000000
+    mukti.bekerja()
+    
+    val burhan = CleaningService()
+    burhan.nama = "burhan"
+    burhan.alamat = "seturan"
+    burhan.sma = "SMA Maarif Yogyakarta"
+    burhan.perkenalan()
+    burhan.gaji = 700000
+    burhan.membersihkan()
+    
+    val koko = PembimbingPramuka()
+    koko.nama = "koko yoi"
+    koko.alamat = "Wonosari"
+    koko.perkenalan()
+    koko.gaji = 4000000
+    koko.pelatih = "Pramuka SMP"
 }
