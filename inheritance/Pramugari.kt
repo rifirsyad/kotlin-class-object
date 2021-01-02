7048e4181fadabf59211177888bc9b9255807aa8
rifirsyad <rifirsyad@gmail.com>
2021-01-02 12:34:49 +0700

    Create Pramugari.kt

---
+++ inheritance/Pramugari.kt
@@ -0,0 +1,10 @@
+package inheritance
+	
+	class Pramugari : Karyawan() {
+	
+	    var maskapai: String = ""
+	
+	    fun bekerja(){
+	        println("Saya bekerja sebagai pramugari di maskapai $maskapai")
+	    }
+	}
